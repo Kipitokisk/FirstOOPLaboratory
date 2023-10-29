24af55bbc22c6b3b206a1c3567a30cb1a80eec4c
Victor Revenco <victor.revenco@isa.utm.md>
2023-10-29 18:32:05 +0200

    Added newfile/deleted

--- src/Lab2/Main.java
+++ src/Lab2/Main.java
@@ -1,10 +1,14 @@
 package Lab2;
+
 import java.io.File;
 import java.util.Scanner;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         CommitAction commitAction = new CommitAction();
+        Set<String> lastSnapshotFiles = new HashSet<>();
 
         String folderPath = "C:\\JavaProjects\\OOP_UTM\\OOPLaboratory\\src\\Lab2\\Files";
         File folder = new File(folderPath);
@@ -26,13 +30,14 @@ public class Main {
             switch (command) {
                 case "commit":
                     commitAction.execute(parts);
+                    lastSnapshotFiles = getSnapshotFiles(folder);
                     break;
                 case "info":
                     InfoAction infoAction = new InfoAction();
                     infoAction.execute(parts);
                     break;
                 case "status":
-                    StatusAction statusAction = new StatusAction(commitAction.getSnapshotTime());
+                    StatusAction statusAction = new StatusAction(commitAction.getSnapshotTime(), lastSnapshotFiles);
                     statusAction.execute(parts);
                     break;
                 case "exit":
@@ -42,5 +47,18 @@ public class Main {
                     System.out.println("Invalid command");
             }
         }
+    }
+
+    private static Set<String> getSnapshotFiles(File folder) {
+        Set<String> snapshotFiles = new HashSet<>();
+        if (folder.exists() && folder.isDirectory()) {
+            File[] files = folder.listFiles();
+            if (files != null) {
+                for (File file : files) {
+                    snapshotFiles.add(file.getName());
+                }
+            }
+        }
+        return snapshotFiles;
     }
 }
--- src/Lab2/StatusAction.java
+++ src/Lab2/StatusAction.java
@@ -2,12 +2,16 @@ package Lab2;
 
 import java.io.File;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 public class StatusAction extends FileAction {
     private Date lastSnapshotTime;
+    private Set<String> lastSnapshotFiles;
 
-    public StatusAction(Date lastSnapshotTime) {
+    public StatusAction(Date lastSnapshotTime, Set<String> lastSnapshotFiles) {
         this.lastSnapshotTime = lastSnapshotTime;
+        this.lastSnapshotFiles = lastSnapshotFiles;
     }
 
     @Override
@@ -20,14 +24,30 @@ public class StatusAction extends FileAction {
             if (files != null) {
                 System.out.println("Created Snapshot at: " + lastSnapshotTime);
 
+                Set<String> currentFiles = new HashSet<>();
                 for (File file : files) {
                     Date lastModifiedTime = new Date(file.lastModified());
-                    String status = (lastModifiedTime.after(lastSnapshotTime)) ? "Changed" : "No Change";
+                    String status = "No Change";
+
+                    if (lastModifiedTime.after(lastSnapshotTime)) {
+                        status = "Changed";
+                    }
+
+                    currentFiles.add(file.getName());
+
+                    if (!lastSnapshotFiles.contains(file.getName())) {
+                        status = "New File";
+                    }
+
                     System.out.println(file.getName() + " - " + status);
+                }
+
+                for (String fileName : lastSnapshotFiles) {
+                    if (!currentFiles.contains(fileName)) {
+                        System.out.println(fileName + " - Deleted File");
+                    }
                 }
             }
         }
     }
 }
-
-
