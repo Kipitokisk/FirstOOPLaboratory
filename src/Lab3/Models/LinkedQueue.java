40dc169cac6c56fd16ce6917d7822ec6f35b63b3
Victor Revenco <victor.revenco@isa.utm.md>
2023-11-18 19:16:21 +0200

    Add linked queue and stack

---
+++ src/Lab3/Models/LinkedQueue.java
@@ -0,0 +1,58 @@
+package Lab3.Models;
+
+public class LinkedQueue implements Queue{
+    private Node front;
+    private Node rear;
+    private int size;
+
+    public LinkedQueue() {
+        front = null;
+        rear = null;
+        size = 0;
+    }
+
+    @Override
+    public void enqueue(int element) {
+        if (size == 5) {
+            System.out.println("Queue is full. Cannot enqueue element.");
+            return;
+        }
+        Node newNode = new Node(element);
+        if (isEmpty()) {
+            front = newNode;
+        } else {
+            rear.next = newNode;
+        }
+        rear = newNode;
+        size++;
+    }
+
+    @Override
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty. Cannot dequeue element.");
+            return -1;
+        }
+        int element = peek();
+        front = front.next;
+        if (front == null) {
+            rear = null; // Queue is now empty
+        }
+        size--;
+        return element;
+    }
+
+    @Override
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty. Cannot peek.");
+            return -1;
+        }
+        return front.data;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
