fc9052e3bad003843bb156f7f846d5fa7bf4366e
Victor Revenco <victor.revenco@isa.utm.md>
2023-11-18 19:01:29 +0200

    Add array up for stack and queue

---
+++ src/Lab3/Models/ArrayUpQueue.java
@@ -0,0 +1,52 @@
+package Lab3.Models;
+
+public class ArrayUpQueue implements Queue{
+    private int[] array;
+    private int front;
+    private int rear;
+    private int size;
+
+    public ArrayUpQueue() {
+        array = new int[5];
+        front = 0;
+        rear = -1;
+        size = 0;
+    }
+
+    @Override
+    public void enqueue(int element) {
+        if (size == 5) {
+            System.out.println("Queue is full. Cannot enqueue element.");
+            return;
+        }
+        rear = (rear + 1) % 5;
+        array[rear] = element;
+        size++;
+    }
+
+    @Override
+    public int dequeue() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty. Cannot dequeue element.");
+            return -1;
+        }
+        int element = peek();
+        front = (front + 1) % 5;
+        size--;
+        return element;
+    }
+
+    @Override
+    public int peek() {
+        if (isEmpty()) {
+            System.out.println("Queue is empty. Cannot peek.");
+            return -1;
+        }
+        return array[front];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+}
